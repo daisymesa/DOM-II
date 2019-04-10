c593af4b19e01c5304d666c92d8278f9ee20b250
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-10 10:39:50 -0400

    Added new listener event, refactored previous code

--- js/index.js
+++ js/index.js
@@ -2,96 +2,102 @@
 
 // 1. mouseover
 const navItems = document.getElementsByClassName('nav-link');
-navItems[0].addEventListener('mouseover', function (event) {
+navItems[0].addEventListener('mouseover', (event) => {
     event.target.style.color = "orange";
 });
-navItems[1].addEventListener('mouseover', function (event) {
+navItems[1].addEventListener('mouseover', (event) => {
     event.target.style.color = "orange";
 });
-navItems[2].addEventListener('mouseover', function (event) {
+navItems[2].addEventListener('mouseover', (event) => {
     event.target.style.color = "orange";
 });
-navItems[3].addEventListener('mouseover', function (event) {
+navItems[3].addEventListener('mouseover', (event) => {
     event.target.style.color = "orange";
 });
 
 // 2. keydown
-window.addEventListener('keydown', function (event) {
+window.addEventListener('keydown', (event) => {
     event.target.style.color = "orange";
 });
 
 // 3. keyup
-window.addEventListener('keyup', function (event) {
+window.addEventListener('keyup', (event) => {
     event.target.style.color = "black";
 });
 
 // 4. wheel
 const firstHeader = document.querySelectorAll(".intro h2");
-firstHeader[0].addEventListener('wheel', function (event) {
+firstHeader[0].addEventListener('wheel', (event) => {
     event.target.style.color = "orange";
 });
 
 // 5. load
-window.addEventListener('load', function (event) {
+window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
 });
 
 // 6. resize
-window.addEventListener('resize', function (event) {
+window.addEventListener('resize', (event) => {
     console.log('page was resized');
 });
 // 7. scroll
-window.addEventListener('scroll', function (event) {
+window.addEventListener('scroll', (event) => {
     console.log('user has scrolled');
 });
 
-// 8. dblclick
-navItems[0].addEventListener("focus", function (event) {
+// 8. focus
+navItems[0].addEventListener("focus", (event) => {
     event.target.style.color = "black";
 });
-navItems[1].addEventListener("focus", function (event) {
+navItems[1].addEventListener("focus", (event) => {
     event.target.style.color = "black";
 });
-navItems[2].addEventListener("focus", function (event) {
+navItems[2].addEventListener("focus", (event) => {
     event.target.style.color = "black";
 });
-navItems[3].addEventListener("focus", function (event) {
+navItems[3].addEventListener("focus", (event) => {
     event.target.style.color = "black";
 });
 
 // 9. click
 const btn = document.getElementsByClassName("btn");
-btn[0].addEventListener("click", function (event) {
+btn[0].addEventListener("click", (event) => {
     event.target.style.color = "black";
     event.target.style.backgroundColor = "gray";
 });
 
-btn[1].addEventListener("click", function (event) {
+btn[1].addEventListener("click", (event) => {
     event.target.style.color = "black";
     event.target.style.backgroundColor = "gray";
 });
-btn[2].addEventListener("click", function (event) {
+btn[2].addEventListener("click", (event) => {
     event.target.style.color = "black";
     event.target.style.backgroundColor = "gray";
 });
 
 // 10. mouseover
 const footer = document.getElementsByTagName("footer");
-footer[0].addEventListener("mouseover", function (event){
-    event.target.style.backgroundColor = "lightyellow";
+footer[0].addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "orange";
+});
+
+// 11. auxClick
+const pTags = document.getElementsByTagName("p");
+pTags[0].addEventListener("auxclick", (event) => {
+    event.target.style.backgroundColor = "black";
+    event.target.style.color = "orange";
 });
 
 
 // Propagation
-const mainContainer = document.querySelector('.main-content');
-mainContainer.addEventListener('click', function(eventObject){
- console.log(`mainContainer fired!`); 
+
+const mainNavigation = document.querySelector('.main-navigation');
+mainNavigation.addEventListener('click', (event) => {
   eventObject.stopPropagation();
 });
 
 // Prevent Default
 const header = document.getElementsByTagName("header");
-header[0].addEventListener("refresh", function (event){
-    console.log("Navigation will not refresh!");
+header[0].addEventListener("refresh", (event) => {
     event.preventDefault();
 });
